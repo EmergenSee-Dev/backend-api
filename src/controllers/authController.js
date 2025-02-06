12b077d3d84772431f02bcc131a4203add50a127
iamalaboexcel <iamalaboexcel@gmail.com>
2025-02-06 14:02:42 +0100

    bug fixes deploy

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,7 +12,7 @@ const authControllers = {
     const { phoneNumber } = req.body;
 
     try {
-      const existingUser = await User.VerifyUser({ phone: phoneNumber });
+      const existingUser = await VerifyUser.findOne({ phone: phoneNumber });
       if (existingUser) {
         return res.status(400).json({ message: "User already exists" });
       }
@@ -29,6 +29,7 @@ const authControllers = {
       res.status(201).json({ message: "User created successfully", user: savedUser });
 
     } catch (error) {
+      console.log(error)
       res.status(500).json({ message: error });
     }
   },
