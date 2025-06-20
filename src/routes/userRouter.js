8957ee74502fbabe0b235dd3a9de4eb75ae16c56
Waves Digital Services <44029351+wdservices@users.noreply.github.com>
2025-06-20 03:47:57 +0100

    Update userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,5 +4,6 @@ const userRouter = express.Router();
 
 userRouter.get("/users", userControllers.getUsers);
 userRouter.get('/user/:id', userControllers.getSingleUser)
+userRouter.delete('/user/:id', userControllers.deleteUser);
 
 module.exports = userRouter;
